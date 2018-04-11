65c22b7fa0582edec899f24fc8dca2fdb5b7e395
fs94007 <33277248+fs94007@users.noreply.github.com>
2018-04-11 11:39:59 +0800

    Update CryptoNoteConfig.h
    Update GENESIS_COINBASE_TX_HEX[]

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "billfengcoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff00010a029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101e83c1d34e4602f4d1f5a58613626e7eb1bf488c2fd9f323dd1a2443339a24099";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
